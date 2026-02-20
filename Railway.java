ba269f713b22b75c5b09311dec7a5356b80a0a82
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-20 14:22:49 +0530

    Add files via upload

---
+++ Railway.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Railway {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int genPass = sc.nextInt();
+        int genFare = sc.nextInt();
+        int acPass= sc.nextInt();
+        int acFare = sc.nextInt();
+        int platform = sc.nextInt();
+        int maintenance = sc.nextInt();
+        int fuel = sc.nextInt();
+
+        int revenue = (genPass * genFare) + (acPass * acFare) + platform - maintenance - fuel;
+        System.out.println(revenue);
+    }
+}
