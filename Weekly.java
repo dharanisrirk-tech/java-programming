852c3d65a23682e7e4a2723eedf325afbcb79576
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-20 09:26:34 +0530

    Add files via upload

---
+++ Weekly.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Weekly{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int days = sc.nextInt();
+        double wage = sc.nextDouble();
+        double bonus = sc.nextDouble();
+        double charge = sc.nextDouble();
+
+        double total = (days * wage) + bonus - charge;
+
+        System.out.println("Final Weekly Wage = " + total);
+    }
+}
+
