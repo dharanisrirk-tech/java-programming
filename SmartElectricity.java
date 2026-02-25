dd5e32476f2d44f9d0e95cf0282d2ca7a308d394
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-25 22:25:02 +0530

    Add files via upload

---
+++ SmartElectricity.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SmartElectricity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int connectionValue = sc.nextInt();
+        int applianceMask = sc.nextInt();
+
+        if ((connectionValue & applianceMask) != 0) {
+            System.out.println("Appliance Active");
+        } else {
+            System.out.println("Appliance Inactive");
+        }
+
+        sc.close();
+    }
+}
