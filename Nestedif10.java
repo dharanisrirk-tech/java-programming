704e02003515bc44778e3acd9211410d2b902c2f
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-03-22 19:29:27 +0530

    Add files via upload

---
+++ Nestedif10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Nestedif10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+
+        System.out.println("Pascal's Triangle:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int s = 0; s < rows - i - 1; s++) {
+                System.out.print(" ");
+            }
+
+            int num = 1; 
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+
+                num = num * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
