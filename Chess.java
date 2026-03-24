e0962e03bce8fbf331c0742e793e693bcf66d2bf
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-03-24 21:02:53 +0530

    Add files via upload

---
+++ Chess.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Chess{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        System.out.println("Chess Board :");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    System.out.print("■ ");
+                } else {
+                    System.out.print("□ ");
+                }
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
