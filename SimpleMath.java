ac563738f1a50ecb8bde574d953e6879f7b5eeda
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-04-30 23:53:02 +0530

    Add files via upload

---
+++ SimpleMath.java
@@ -0,0 +1,12 @@
+public class SimpleMath {
+    public static void main(String[] args) {
+        int a = 125;
+        int b = 24;
+        int difference = a - b;
+        int product = a * b;
+
+        System.out.println("125 - 24 = " + difference);
+        System.out.println("125 * 24 = " + product);
+    }
+}
+
