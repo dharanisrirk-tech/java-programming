9a5cb3ea09521833234a560be39d3aeeb4314c5e
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-05-07 23:32:46 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // Found a factor, so it's not prime
+            }
+        }
+        return true;
+    }
+}
