5dae655bbff4662c135e4f15fbf2043a5a95803f
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-05-11 22:03:46 +0530

    Add files via upload

---
+++ SpecialCharacter.java
@@ -0,0 +1,12 @@
+public class SpecialCharacter{
+    public static void main(String[] args) {
+        // Common escape sequences
+        System.out.println("New line: First line\nSecond line");
+        System.out.println("Tabbed: Column1\tColumn2");
+        System.out.println("Quotes: \"Hello World\"");
+        System.out.println("Backslash: C:\\Users\\Name");
+    }
+}
+
+
+
