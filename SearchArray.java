d2d2a96bc0b53cb80c992451f2c5a2454d86c39c
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-05-17 22:03:56 +0530

    Add files via upload

---
+++ SearchArray.java
@@ -0,0 +1,19 @@
+public class SearchArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+        int foundIndex = -1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                foundIndex = i;
+                break; 
+            }
+        }
+
+        if (foundIndex != -1) {
+            System.out.println("Element " + target + " found at index: " + foundIndex);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
